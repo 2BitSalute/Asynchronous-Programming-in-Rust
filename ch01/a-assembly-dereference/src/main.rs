fbcce5a3c02e187db7a39f8513627b1374f9b20b
2BitSalute <tatianaracheva@gmail.com>
2024-03-03 15:44:05 -0800

    Conditional `mov` template syntax on different architectures

--- ch01/a-assembly-dereference/src/main.rs
+++ ch01/a-assembly-dereference/src/main.rs
@@ -13,8 +13,12 @@ fn main() {
 
 fn dereference(ptr: *const usize) -> usize {
     let mut res: usize;
-    unsafe { 
-        asm!("mov {0}, [{1}]", out(reg) res, in(reg) ptr)
+    unsafe {
+        #[cfg(target_arch = "x86_64")]
+        asm!("mov {0}, [{1}]", out(reg) res, in(reg) ptr);
+
+        #[cfg(target_arch = "aarch64")]
+        asm!("mov {0}, {1}", out(reg) res, in(reg) ptr)
     };
     res
 }
